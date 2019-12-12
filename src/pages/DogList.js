d90c8c0f6bf5ba4ca8b67156e357b2e7166dc51d
hemantkumar2 <hemantnirmalkar17@gmail.com>
2019-12-12 18:01:59 +0530

    style added on dog images and links, made dog link name dynamic

--- src/pages/DogList.js
+++ src/pages/DogList.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react'
 import './DogsList.css'
+import { Link } from 'react-router-dom'
 export class DogList extends Component {
 
   render() {
     return (
       <div>
+        <div className='display-3'>Dogs List</div>
         <div className="dog-list mt-5">
           <div className="container">
             <div className="row no-gutters">
               {this.props.dogs.map(d => (
                 <div className="dog col-lg-4 text-center">
                   <div key={d.name}>
-                    <img src={d.src} alt={d.name} />
-                    <h1>{d.name}</h1>
+                    <img src={d.src} alt={d.name} /><br />
+                    <Link className='dog-details-link' exact to={`/dogs/${d.name}`}>{d.name}</Link>
                   </div>
                 </div>
               ))}
             </div>
           </div>
         </div>
-      </div>
+      </div >
     )
   }
 }
