04d2777b55ecf0be870cfb775d62f13df75bedaa
hemantkumar2 <hemantnirmalkar17@gmail.com>
2019-12-12 11:40:51 +0530

    added array and images for dogs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import DogList from './pages/DogsList'
+import whiskey from './assets/whiskey.jpg'
+import hazel from './assets/whiskey.jpg'
+import tubby from './assets/whiskey.jpg'
+import Navbar from './Components/Navbar'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: whiskey,
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: hazel,
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: tubby,
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  }
 
-    </div>
-  );
+  render() {
+    return (
+      <div className="App" >
+        <Navbar />
+        <Switch>
+          <Route path='/dogs' render={() => <DogList dogs={this.props.dogs} />} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
